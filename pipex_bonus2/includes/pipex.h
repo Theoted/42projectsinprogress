82fa9ede4f8498e0288f070e2b840317b95c5037
Theo Deville <tdeville@z2r10p8.42lyon.fr>
2022-02-22 10:37:12 +0100

    multipipe finished

---
+++ pipex_bonus2/includes/pipex.h
@@ -0,0 +1,59 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tdeville <tdeville@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/29 11:10:16 by tdeville          #+#    #+#             */
+/*   Updated: 2022/02/22 10:33:52 by tdeville         ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PIPEX_H
+# define PIPEX_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <sys/types.h>
+# include <sys/wait.h>
+# include <fcntl.h>
+# include <string.h>
+# include "../libft/libft.h"
+
+typedef struct s_pipex
+{
+	int		infile;
+	int		outfile;
+	pid_t	*pid;
+	int		*fd;
+	char	*path;
+	char	**s_path;
+	char	*cmd;
+	char	**arg;
+	int		nb_cmd;
+}			t_pipex;
+
+// main
+char		*find_cmd(char **arg_vec, t_pipex data);
+char		*find_path(char **envp);
+
+// Init
+int			open_pipes(t_pipex *data);
+int			open_files(t_pipex *data, char **av, int ac);
+int			data_init(t_pipex *data, char **av, int ac, char **envp);
+
+// Access
+int			check_access(char **arg_vec, char **arg);
+
+//utils
+void		free_all(char **str);
+int			error(char *err);
+int			double_arrlen(char **str);
+void		del(char *str);
+void		last_free(t_pipex *data);
+void		free_in_process(char **arg, t_pipex *data);
+void		arg_error(char *err);
+
+#endif
---
+++ pipex_bonus2/src/init.c
@@ -0,0 +1,73 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   init.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tdeville <tdeville@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/21 15:09:37 by tdeville          #+#    #+#             */
+/*   Updated: 2022/02/21 16:07:27 by tdeville         ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/pipex.h"
+
+int	open_pipes(t_pipex *data)
+{
+	int	i;
+	int	j;
+
+	i = -1;
+	j = -1;
+	while (++i < (data->nb_cmd - 1))
+	{	
+		if (pipe(&data->fd[i * 2]) == -1)
+		{
+			while (++j < i)
+				close(data->fd[j]);
+			return (error("Pipe error\n"));
+		}
+	}
+	return (0);
+}
+
+int	open_files(t_pipex *data, char **av, int ac)
+{
+	data->infile = open(av[1], O_RDONLY, 0644);
+	if (data->infile == -1)
+		return (error("infile not found\n"));
+	data->outfile = open(av[ac - 1], O_RDWR | O_CREAT, 0644);
+	if (data->outfile == -1)
+	{
+		close(data->infile);
+		return (error("open error\n"));
+	}
+	return (0);
+}
+
+int	data_init(t_pipex *data, char **av, int ac, char **envp)
+{
+	if (open_files(data, av, ac) == 1)
+		return (1);
+	data->nb_cmd = (ac - 3);
+	data->pid = malloc(sizeof(pid_t) * data->nb_cmd);
+	if (!data->pid)
+	{
+		close(data->infile);
+		close(data->outfile);
+		return (error("Malloc error\n"));
+	}
+	data->fd = malloc(sizeof(int) * ((data->nb_cmd - 1) * 2));
+	if (!data->fd)
+	{
+		close(data->infile);
+		close(data->outfile);
+		free(data->pid);
+		return (error("Malloc error\n"));
+	}
+	data->path = find_path(envp);
+	data->s_path = ft_split(data->path, ':');
+	free(data->path);
+	open_pipes(data);
+	return (0);
+}
